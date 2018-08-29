8222fc8aa300afe2b42180d869409a8c165ecaaf
Wendy Mak <wwymak@gmail.com>
2018-08-29 21:43:56 +0100

    rotated binary search in python-- still a bit buggy...

---
+++ rotated_binary_sort.py
@@ -0,0 +1,62 @@
+
+
+def binary_search(mlist, a):
+
+    start = 0
+    end = len(mlist) -1
+
+    while start <= end:
+        middle = (start + end)//2
+        if a == mlist[middle]:
+            return middle
+        elif a > mlist[middle]:
+            start = middle + 1
+        else:
+            end = middle -1
+    return -1
+
+def custom_binary_search(mlist, a):
+    """
+
+    :param mlist: rotated sorted list
+    :param a: nummber to look for
+    :return: index of a
+    """
+    start = 0
+    end = len(mlist) - 1
+
+    while start <= end:
+        middle = (start + end) // 2
+        if a == mlist[middle]:
+            return middle
+        #the left side is in the correct order
+        elif mlist[start] < mlist[middle]:
+            if a <mlist[middle] :
+                return binary_search(mlist[start: middle], a)
+            else:
+                return middle + custom_binary_search(mlist[middle:], a)
+
+        #the right side is in correct order
+        else:
+            if mlist[middle] < a < mlist[len(mlist) -1]:
+                return middle + binary_search(mlist[middle: len(mlist) -1], a)
+            else:
+                return custom_binary_search(mlist[start: middle], a)
+
+    return -1
+
+
+if __name__ == "__main__":
+
+    mlist = [ 1,  2, 15, 24, 24, 30, 47, 54, 68, 71, 76, 76, 83, 83, 83, 83, 84, 94, 99]
+    rotate_mmlist = [76, 76, 83, 83, 83, 83, 84, 94, 99, 1,  2, 15, 24, 24, 30, 47, 54, 68, 71]
+
+    for i, a in enumerate(mlist):
+        idx = binary_search(mlist, a)
+        print(f"a: {a}, idx: {idx}, actual: {i}")
+
+    for i, a in enumerate(rotate_mmlist):
+        idx2 = custom_binary_search(rotate_mmlist, a)
+        print(f"a: {a}, idx: {idx2}, actual: {i}")
+
+
